66126da7112878230f3a52f99d97dad8a1cd01b6
Morgan Vaterkowski <morgan.vaterkowski@epita.fr>
2021-10-11 17:53:05 +0200

    working ammeter

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ pin_adc_min = machine.Pin("S19", machine.Pin.IN)
 adc_pos = machine.ADC(pin_adc_pos)
 adc_min = machine.ADC(pin_adc_min)
 resistance = 22
+
 while True:
-    print(((adc_min.read_u16() - adc_pos.read_u16()) * 0.00488) / resistance)
+    print("value = " + str((adc_min.read_u16() * 3.3) / 65536))
+    #print(((adc_min.read_u16() - adc_pos.read_u16()) * 0.00488) / resistance)
