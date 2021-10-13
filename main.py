e35f7ee12bf8a270a531aabf69bb5ae59b88b387
Morgan Vaterkowski <morgan.vaterkowski@epita.fr>
2021-10-13 17:50:27 +0200

    main: a measure every 5 minutes and can cancel every 5

--- main.py
+++ main.py
@@ -3,20 +3,26 @@ import time
 
 import machine
 
+###
 sw = pyb.Switch()
+ite = 0
+### get led ready
 pyb_leds = [pyb.LED(1), pyb.LED(2), pyb.LED(3), pyb.LED(4)]
-
+### reset call back in case of button press
+sw.callback(None)
+### Waiting for the button to start the measure
 while not sw():
     pyb_leds[0].on()
     pyb_leds[1].on()
 
+## switch off led and wait two seconds before starting to avoid the button to be reconnized as a stop signal
 pyb_leds[0].off()
 pyb_leds[1].off()
+time.sleep(2)
 
 ### Generate the csv file without ereasing other measurement
 a = 0
 file = None
-time.sleep(2)
 while file is None:
     try:
         file = open("/sd/data/mesure" + str(a) + ".csv", "x")
@@ -29,16 +35,18 @@ file.flush()
 
 pin_adc_pos = machine.Pin("S23", machine.Pin.IN)
 pin_adc_min = machine.Pin("S19", machine.Pin.IN)
-
 adc_min = machine.ADC(pin_adc_min)
 adc_max = machine.ADC(pin_adc_pos)
-resistance = 22
+
+
+### def a func to be clal on button press to close file and warn people
+### that the usb can be removed without losing data
 
 for led in pyb_leds:
     led.on()
 
 t = time.time()
-
+t2 = time.time()
 while not sw():
     pyb_leds[0].on()
     pyb_leds[1].on()
@@ -48,25 +56,33 @@ while not sw():
 
     ### maj of csv
 
-    file.write("\n")
+
     second_pasted = time.time() - t
     hours = second_pasted / 3600
     minutes = (second_pasted - (hours * 3600)) / 60
     secondes = second_pasted - (second_pasted / 60)
 
+    if (time.time() - t2 > 300):
+        file.write("\n")
+        file.write(str(value_max) + ';' + str(value_min) + ';' + str(value_max - value_min)+";" + str(time.time() - t))
+        t2 = time.time()
+
     ### écrit en secondes
-    file.write(str(value_max) + ';' + str(value_min) + ';' + str(value_max - value_min)+";" + str(time.time() - t))
 
     ### écrit en xxhxxmxxhsecondes
     #file.write(str(value_max) + ';' + str(value_min) + ';' + str(value_max - value_min) + ";" + str(hours) + "H " + str(
-    #0    minutes) + "min " + str(secondes) + "sec")
+    #    minutes) + "min " + str(secondes) + "sec")
+
+
     ###Stop ! wait a minute
 
-    time.sleep(9)
+    time.sleep(4)
     pyb_leds[0].off()
     pyb_leds[1].off()
     time.sleep(1)
 
+
 file.close()
 for led in pyb_leds:
     led.off()
+
